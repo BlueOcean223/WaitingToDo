258433674334a3642e50e56de3d7808d96edc95d
BlueOcean223 <1751259208@qq.com>
2025-05-11 23:47:44 +0800

    初始化项目

---
+++ back/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatal("程序启动失败")
+	}
+}
