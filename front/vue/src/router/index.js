efede72feae29a02b61a75cce202c0132ba1bb8f
BlueOcean223 <1751259208@qq.com>
2025-06-06 18:16:20 +0800

    添加了路由守卫

--- front/vue/src/router/index.js
+++ front/vue/src/router/index.js
@@ -25,15 +25,15 @@ const router = createRouter({
 });
 
 // 路由守卫，确保用户未登录时无法访问其他页面
-// router.beforeEach((to, from, next) => {
-//     const isLogin = localStorage.getItem("username");
-//     if (isLogin == null && to.path !== "/login") {
-//       next("/login");
-//     } else if(isLogin != null && to.path === "/login") {
-//       next("/main");
-//     }else{
-//       next();
-//     }
-//   });
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.getItem("user");
+  if (isLogin == null && to.path !== "/login") {
+    next("/login");
+  } else if(isLogin != null && to.path === "/login") {
+    next("/home");
+  }else{
+    next();
+  }
+});
 
 export default router;
